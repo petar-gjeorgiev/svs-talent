e04fa2095923bdc6cddb5f636eb6e37e3755920d
gjeorgiev-petar <petar_1992georgiev>
2015-03-12 19:39:38 +0100

    Simple change

--- SVS-HOMEWORK/src/ClassOrganisation/ZipCodeMain.java
+++ SVS-HOMEWORK/src/ClassOrganisation/ZipCodeMain.java
@@ -5,5 +5,6 @@ public class ZipCodeMain {
 	public static void main(String[] args) {
 		ZipCode zipCode = new ZipCode(1234);
 		System.out.println(zipCode.getZipCode());
+		System.out.println("Test 1");
 	}
 }
